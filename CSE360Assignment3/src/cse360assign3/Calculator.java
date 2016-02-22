77c5e232d2f21d9c87bb6756b06668eadef0227f
Aaron Prieger <aprieger@asu.edu>
2016-02-21 22:27:48 -0700

    Create the initial version

---
+++ CSE360Assignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,80 @@
+
+package cse360assign3;
+
+/** Calculator: Class that contains seven methods, to create Calculator, get the total, add a
+ * value to the total, subtract a value from the total, multiply a value by the total, divide
+ * a value by the total, and a method to return the history of the actions.
+ * @author Aaron
+ * @version:	Assignment 3.1
+ */
+public class Calculator {
+
+	private int total;
+	
+	
+	/** Calculator () : create calculator object	
+	 * */
+	public Calculator ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/** getTotal () : return the total
+	 * @return int total	
+	 * */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	
+	/** add () : adds the value to the total
+	 * 
+	 * @param value to be added
+	 * */
+	public void add (int value)
+	{
+		
+	}
+	
+	
+	/** subtract () : subtracts the value to the total
+	 * 
+	 * @param value to be subtracted
+	 * */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	
+	/** multiply () : multiplies the value by the total
+	 * 
+	 * @param value to be multiplied
+	 * */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	
+	/** divide () : divides the value by the total
+	 * 
+	 * @param value to be divided
+	 * */
+	public void divide (int value)
+	{
+		
+	}
+	
+	
+	/** getHistory () : subtracts the value to the total
+	 * 
+	 * @return string value of history of actions
+	 * */
+	public String getHistory ()
+	{
+		return "";
+	}
+}
